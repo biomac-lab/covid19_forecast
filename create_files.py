73de9496211caa5227ad5a7ba80ee854b0e7fd2b
Jaime Cascante <je.cascante10@uniandes.edu.co>
2021-02-02 03:53:06 -0500

    adding estimates

--- create_files.py
+++ create_files.py
@@ -39,7 +39,18 @@ data = data.rename(columns={'num_cases': 'confirmed', 'num_diseased':'death'})[[
 data = prepare_cases(data, col='confirmed')
 data = prepare_cases(data, col='death')
 
-data = data.iloc[:-11]
+data = data.iloc[:-14]
+
+
+
+T_future = 27
+path_to_save = os.path.join(results_dir, 'weekly_forecast' , 'bogota', 'smoohted_'+pd.to_datetime(data.index.values[-1]).strftime('%Y-%m-%d'))
+
+x = np.load(os.path.join(path_to_save,'samples.npz'), allow_pickle=True)
+
+mcmc_samples, post_pred_samples, forecast_samples = load_samples(os.path.join(path_to_save,'samples.npz'))
+
+
 
 model = SEIRD(
     confirmed = data['confirmed'].cumsum(),
@@ -48,21 +59,11 @@ model = SEIRD(
     N         = 8181047,
     )
 
-
-T_future = 27
-path_to_save = os.path.join(results_dir, 'weekly_forecast' , 'bogota', pd.to_datetime(data.index.values[-1]).strftime('%Y-%m-%d'))
-
-x = np.load(os.path.join(path_to_save,'samples.npz'), allow_pickle=True)
-
-mcmc_samples, post_pred_samples, forecast_samples = load_samples(os.path.join(path_to_save,'samples.npz'))
-
-fitted_deaths = np.concatenate(samples["mean_dz0"], samples["mean_dz"].numpy())
-
 forecast_samples['mean_dz0'] = forecast_samples["dz0"]
 forecast_samples['mean_dy0'] = forecast_samples["dy0"]
 
-deaths_fitted = model.combine_samples(mcmc_samples, f='mean_dz', use_future=False)
-cases_fitted  = model.combine_samples(mcmc_samples, f='mean_dy', use_future=False)
+deaths_fitted = model.combine_samples(forecast_samples, f='mean_dz', use_future=True)
+cases_fitted  = model.combine_samples(forecast_samples, f='mean_dy', use_future=True)
 
 def create_df_response(samples, time, date_init ='2020-03-06',  forecast_horizon=27, use_future=False):
 
@@ -80,104 +81,45 @@ def create_df_response(samples, time, date_init ='2020-03-06',  forecast_horizon
     df_response['mean']        = results_df.mean(axis=1).values
     df_response['median']      = results_df.median(axis=1).values
     df_response['std']         = results_df.std(axis=1).values
+    df_response['low_975']      = results_df.quantile(q=0.025, axis=1).values
+    df_response['high_975']     = results_df.quantile(q=0.975, axis=1).values
+
     df_response['low_90']      = results_df.quantile(q=0.1, axis=1).values
     df_response['high_90']     = results_df.quantile(q=0.9, axis=1).values
-    df_response['type']        =  types
 
+    df_response['low_75']      = results_df.quantile(q=0.25, axis=1).values
+    df_response['high_75']     = results_df.quantile(q=0.75, axis=1).values
+
+    df_response['type']        =  types
+    df_response.index.name = 'date'
     return df_response
 
-df_deaths = create_df_response(deaths_fitted, time=len(data), date_init ='2020-03-06',  forecast_horizon=27, use_future=False)
-df_cases  = create_df_response(cases_fitted, time=len(data), date_init ='2020-03-06',  forecast_horizon=27, use_future=False)
+df_deaths = create_df_response(deaths_fitted, time=len(data), date_init ='2020-03-06',  forecast_horizon=27, use_future=True)
+df_cases  = create_df_response(cases_fitted, time=len(data), date_init ='2020-03-06',  forecast_horizon=27, use_future=True)
+
+df_deaths.to_csv(os.path.join(path_to_save, 'deaths_df.csv'))
+df_cases.to_csv(os.path.join(path_to_save, 'cases_df.csv'))
 
 
 
 
-fig, ax = plt.subplots(1, 1, figsize=(15.5, 7))
-#ax.plot(pd.date_range(start='2020-03-06', periods=deaths_loc.shape[-1]), np.squeeze(np.median(deaths_loc,1)), color='k')
-ax.scatter(data.index.values, data.smoothed_death, facecolor='black', alpha=0.6, edgecolor='black')
-ax.fill_between(df_deaths.index.values, df_deaths.low_90, df_deaths.high_90, color='gray', alpha=0.4, label='95 CI - Nowcast')
-ax.plot(df_deaths.index.values, df_deaths["median"], color='black', alpha=0.4, label='Median - Nowcast')
 
-ax.fill_between(df_deaths_forecast.index.values, df_deaths_forecast.low_ci_025, df_deaths_forecast.high_ci_975, color='blue', alpha=0.6, label='4 week forecast')
-ax.plot(df_deaths_forecast.index.values, df_deaths_forecast.deaths_mean, color='blue', alpha=0.4, label='Forecast - Median')
-ax.scatter(df_deaths_forecast.index.values, df_deaths_forecast.deaths_mean, edgecolor='k', facecolor='white')#, label='Deaths')
-
-#ax.plot(dates_forecast, median[num_times-1:num_times+num_forecast-1], color='darksalmon', linestyle='--')
-#ax.scatter(dates_forecast, median[num_times-1:num_times+num_forecast-1], edgecolor='k', facecolor='white')#, label='Deaths')
-ax.xaxis.set_major_locator(mdates.MonthLocator())
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
-ax.xaxis.set_minor_locator(mdates.DayLocator())
-ax.xaxis.set_major_locator(mdates.WeekdayLocator())
-ax.xaxis.set_major_locator(mdates.MonthLocator())
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.spines['left'].set_visible(False)
-ax.spines['bottom'].set_visible(False)
-ax.grid(which='major', axis='y', c='k', alpha=.1, zorder=-2)
-ax.tick_params(axis='both', labelsize=15)
-ax.yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.0f}"))
-#ax.axvline(x = 37, linestyle='--',  label = '{}'.format(dates[-1].strftime('%b-%d')))
-ax.set_ylabel('Deaths', size=15)
-ax.legend()
-
-fig.savefig(os.path.join(path_to_save, 'death_aggregated_forecast_mcmc.png'), dpi=300, bbox_inches='tight', transparent=False)
-df_deaths_all = pd.concat([df_deaths, df_deaths_forecast])
-df_deaths_all.to_csv(os.path.join(path_to_save, 'deaths_forecast.csv'))
-plt.close()
-
-
-################################################  CASES ################################################
-
-
-fig, ax = plt.subplots(1, 1, figsize=(15.5, 7))
-#ax.plot(pd.date_range(start='2020-03-06', periods=deaths_loc.shape[-1]), np.squeeze(np.median(deaths_loc,1)), color='k')
-ax.scatter(data.index.values, data.smoothed_confirmed, facecolor='black', alpha=0.6, edgecolor='black')
-ax.fill_between(df_deaths.index.values, df_cases.low_90, df_cases.high_90, color='gray', alpha=0.4, label='95 CI - Nowcast')
-ax.plot(df_cases.index.values, df_cases["median"], color='black', alpha=0.4, label='Median - Nowcast')
-
-ax.fill_between(df_deaths_forecast.index.values, df_deaths_forecast.low_ci_025, df_deaths_forecast.high_ci_975, color='green', alpha=0.4, label='4 week forecast')
-ax.plot(df_deaths_forecast.index.values, df_deaths_forecast.cases_mean, color='green', alpha=0.4, label='Forecast - Median')
-ax.scatter(df_deaths_forecast.index.values, df_deaths_forecast.cases_mean, edgecolor='k', facecolor='white')#, label='Deaths')
-
-ax.xaxis.set_major_locator(mdates.MonthLocator())
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%b'))
-ax.xaxis.set_minor_locator(mdates.DayLocator())
-ax.xaxis.set_major_locator(mdates.WeekdayLocator())
-ax.xaxis.set_major_locator(mdates.MonthLocator())
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.spines['left'].set_visible(False)
-ax.spines['bottom'].set_visible(False)
-ax.grid(which='major', axis='y', c='k', alpha=.1, zorder=-2)
-ax.tick_params(axis='both', labelsize=15)
-ax.yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.0f}"))
-#ax.axvline(x = 37, linestyle='--',  label = '{}'.format(dates[-1].strftime('%b-%d')))
-ax.set_ylabel('Cases', size=15)
-ax.legend()
-fig.savefig(os.path.join(path_to_save, 'cases_aggregated_forecast_mcmc.png'), dpi=300, bbox_inches='tight', transparent=False)
-df_deaths_all = pd.concat([df_deaths, df_deaths_forecast])
-df_deaths_all.to_csv(os.path.join(path_to_save, 'cases_forecast.csv'))
-plt.close()
-
-
-
-
-# Compute growth rate over time
-beta  = mcmc_samples['beta']
-sigma = mcmc_samples['sigma'][:,None]
-gamma = mcmc_samples['gamma'][:,None]
-
-# compute Rt
-rt = SEIRModel.R0((beta, sigma, gamma))
-results_df = pd.DataFrame(rt.T)
-
-df_rt = pd.DataFrame(index=t)
-
-# Calculate key statistics
-df_rt['mean']        = results_df.mean(axis=1).values
-df_rt['median']      = results_df.median(axis=1).values
-df_rt['sdv']         = results_df.std(axis=1).values
-df_rt['low_ci_05']   = results_df.quantile(q=0.05, axis=1).values
-df_rt['high_ci_95']  = results_df.quantile(q=0.95, axis=1).values
-df_rt['low_ci_025']  = results_df.quantile(q=0.025, axis=1).values
-df_rt['high_ci_975'] = results_df.quantile(q=0.975, axis=1).values
+## Compute growth rate over time
+#beta  = mcmc_samples['beta']
+#sigma = mcmc_samples['sigma'][:,None]
+#gamma = mcmc_samples['gamma'][:,None]
+#
+## compute Rt
+#rt = SEIRModel.R0((beta, sigma, gamma))
+#results_df = pd.DataFrame(rt.T)
+#
+#df_rt = pd.DataFrame(index=t)
+#
+## Calculate key statistics
+#df_rt['mean']        = results_df.mean(axis=1).values
+#df_rt['median']      = results_df.median(axis=1).values
+#df_rt['sdv']         = results_df.std(axis=1).values
+#df_rt['low_ci_05']   = results_df.quantile(q=0.05, axis=1).values
+#df_rt['high_ci_95']  = results_df.quantile(q=0.95, axis=1).values
+#df_rt['low_ci_025']  = results_df.quantile(q=0.025, axis=1).values
+#df_rt['high_ci_975'] = results_df.quantile(q=0.975, axis=1).values
