2ed3c1aac9fa485fafb2055872e85e39e2c614cb
Jaime Cascante <je.cascante10@uniandes.edu.co>
2021-02-01 21:37:23 -0500

    correcting fields to save

--- global_config/config.py
+++ global_config/config.py
@@ -11,7 +11,7 @@ current_location = Path(os.path.realpath(__file__)).parent
 # Loads
 try:
     df_config = pd.read_csv(os.path.join(current_location, 'config_file.csv'), index_col = 'name')
-    
+
 except FileNotFoundError:
     raise ValueError('The configuration file: "config_file.csv", was not found insise the directory: "global_config/". Please add it')
 
